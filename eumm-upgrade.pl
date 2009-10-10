a71a9a82d205e602d83943a86d106cf258c0c961
Alexandr Ciornii <alexchorny@gmail.com>
2009-10-11 02:09:18 +0300

    check Makefile.PL

--- eumm-upgrade.pl
+++ eumm-upgrade.pl
@@ -7,3 +7,9 @@ use Perl6::Say;
 use File::Slurp;
 require Module::Install::Repository;
 my $content=read_file('Makefile.PL') or die "Cannot find 'Makefile.PL'";
+if ($content =~ /use inc::Module::Install/) {
+  die "Module::Install is used, no need to upgrade";
+}
+if ($content !~ /use ExtUtils::MakeMaker/ or $content !~ /WriteMakefile\s+\(/) {
+  die "ExtUtils::MakeMaker is not used";
+}
