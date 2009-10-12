63002ce792d950456d2f4218c240b57ec8c6018f
Alexandr Ciornii <alexchorny@gmail.com>
2009-10-12 20:21:14 +0300

    extract code to sub process_file

--- eumm-upgrade.pl
+++ eumm-upgrade.pl
@@ -8,6 +8,7 @@ use File::Slurp;
 require Module::Install::Repository;
 require Module::Install::Metadata;
 use Text::FindIndent;
+
 my $content=read_file('Makefile.PL') or die "Cannot find 'Makefile.PL'";
 if ($content =~ /use inc::Module::Install/) {
   die "Module::Install is used, no need to upgrade";
@@ -19,41 +20,44 @@ if ($content =~ /WriteMakefile1\s*\(/) {
 if ($content !~ /use ExtUtils::MakeMaker/ or $content !~ /WriteMakefile\s*\(/) {
   die "ExtUtils::MakeMaker is not used";
 }
-my $indentation_type = Text::FindIndent->parse($content);
-my $space_to_use;
-if ($indentation_type =~ /^[sm](\d+)/) {
-  print "Indentation with $1 spaces\n";
-  $space_to_use=$1;
-} elsif ($indentation_type =~ /^t(\d+)/) {
-  print "Indentation with tabs, a tab should indent by $1 characters\n";
-  $space_to_use=0;
-} else {
-  print "Indentation unknown, will use 4 spaces\n";
-  $space_to_use=4;
-}
 
-sub apply_indent {
+sub process_file {
   my $content=shift;
-  my $i_from=shift || die;
-  my $i_to=shift;
-  sub _do_replace {
-    my $spaces=shift;
-    my $i_from=shift;
-    my $i_to=shift;
-    my $len=length($spaces);
-    my $l1=int($len/$i_from);
-    if ($i_to==0) {
-      return "\t"x$l1;
-    } else {
-      return " " x ($l1*$i_to);
-    }
+  my $indentation_type = Text::FindIndent->parse($content);
+  my $space_to_use;
+  if ($indentation_type =~ /^[sm](\d+)/) {
+    print "Indentation with $1 spaces\n";
+    $space_to_use=$1;
+  } elsif ($indentation_type =~ /^t(\d+)/) {
+    print "Indentation with tabs, a tab should indent by $1 characters\n";
+    $space_to_use=0;
+  } else {
+    print "Indentation unknown, will use 4 spaces\n";
+    $space_to_use=4;
   }
-  $content=~s/^((?:[ ]{$i_from})+)/_do_replace($1,$i_from,$i_to)/emg;
-  return $content;
-}
 
-my $compat_layer=<<'EOT';
-sub WriteMakefile1 {  #Written by Alexandr Ciornii, version 0.20
+  sub apply_indent {
+    my $content=shift;
+    my $i_from=shift || die;
+    my $i_to=shift;
+    sub _do_replace {
+      my $spaces=shift;
+      my $i_from=shift;
+      my $i_to=shift;
+      my $len=length($spaces);
+      my $l1=int($len/$i_from);
+      if ($i_to==0) {
+        return "\t"x$l1;
+      } else {
+        return " " x ($l1*$i_to);
+      }
+    }
+    $content=~s/^((?:[ ]{$i_from})+)/_do_replace($1,$i_from,$i_to)/emg;
+    return $content;
+  }
+
+  my $compat_layer=<<'EOT';
+sub WriteMakefile1 {  #Written by Alexandr Ciornii, version 0.20. Added by eumm-upgrade.
     my %params=@_;
     my $eumm_version=$ExtUtils::MakeMaker::VERSION;
     $eumm_version=eval $eumm_version;
@@ -78,87 +82,87 @@ sub WriteMakefile1 {  #Written by Alexandr Ciornii, version 0.20
     WriteMakefile(%params);
 }
 EOT
-my $space=' 'x4;
-$content=~s/
-\(\$\]\s*>=\s*5\.005\s*\?\s*\#\#\s*\QAdd these new keywords supported since 5.005\E\s*
-\s+\Q(ABSTRACT_FROM  => '\E([^'\n]+)',\s*\#\s*\Qretrieve abstract from module\E\s*
-\s+AUTHOR\s*=>\s*'([^'\n]+)\Q') : ()),\E\s+
-/ABSTRACT_FROM => '$1',\n${space}AUTHOR => '$2',\n/sx;
+  my $space=' 'x4;
+  $content=~s/
+  \(\$\]\s*>=\s*5\.005\s*\?\s*\#\#\s*\QAdd these new keywords supported since 5.005\E\s*
+  \s+\Q(ABSTRACT_FROM  => '\E([^'\n]+)',\s*\#\s*\Qretrieve abstract from module\E\s*
+  \s+AUTHOR\s*=>\s*'([^'\n]+)\Q') : ()),\E\s+
+  /ABSTRACT_FROM => '$1',\n${space}AUTHOR => '$2',\n/sx;
 
-$content=~s/
-	\(\s*\$ \QExtUtils::MakeMaker::VERSION ge '\E [\d\._]+ \Q' ? (\E \s+
-	( [^()]+? ) \s+
-	\)\s*\:\s*\(\)\s*\),
-/$1/sxg;
+  $content=~s/
+          \(\s*\$ \QExtUtils::MakeMaker::VERSION ge '\E [\d\._]+ \Q' ? (\E \s+
+          ( [^()]+? ) \s+
+          \)\s*\:\s*\(\)\s*\),
+  /$1/sxg;
 
-my @param;
+  my @param;
 
-my @resourses;
-my $repo = Module::Install::Repository::_find_repo(\&Module::Install::Repository::_execute);
-if ($repo and $repo=~m#://#) {
-  print "Repository found: $repo\n";
-  push @resourses,"${space}${space}${space}repository => '$repo',";
-}
+  my @resourses;
+  my $repo = Module::Install::Repository::_find_repo(\&Module::Install::Repository::_execute);
+  if ($repo and $repo=~m#://#) {
+    print "Repository found: $repo\n";
+    push @resourses,"${space}${space}${space}repository => '$repo',";
+  }
 
-if ($content=~/\bVERSION_FROM['"]?\s*=>\s*'([^'\n]+)'/) {
-  my $main_file=$1;
-  my $main_file_content=eval { read_file($1) };
-  if (!$main_file_content) {
-    print "Cannot open $main_file\n";
-  } else {
-    my @links=Module::Install::Metadata::_extract_bugtracker($main_file_content);
-    if (@links==1) {
-      my $bt=$links[0];
-      print "Bugtracker found: $bt\n";
-      push @resourses,"${space}${space}${space}bugtracker => '$bt',";
-    } elsif (@links>1) {
-      print "Too many links to bugtrackers found in $main_file\n";
-    }
-    if ($content !~ /\bLICENSE\s*=>\s*['"]/) {
-      my $l=Module::Install::Metadata::_extract_license($main_file_content);
-      if ($l) {
-        push @param,"    LICENSE => '$l',\n";
+  if ($content=~/\bVERSION_FROM['"]?\s*=>\s*'([^'\n]+)'/) {
+    my $main_file=$1;
+    my $main_file_content=eval { read_file($1) };
+    if (!$main_file_content) {
+      print "Cannot open $main_file\n";
+    } else {
+      my @links=Module::Install::Metadata::_extract_bugtracker($main_file_content);
+      if (@links==1) {
+        my $bt=$links[0];
+        print "Bugtracker found: $bt\n";
+        push @resourses,"${space}${space}${space}bugtracker => '$bt',";
+      } elsif (@links>1) {
+        print "Too many links to bugtrackers found in $main_file\n";
+      }
+      if ($content !~ /\bLICENSE\s*=>\s*['"]/) {
+        my $l=Module::Install::Metadata::_extract_license($main_file_content);
+        if ($l) {
+          push @param,"    LICENSE => '$l',\n";
+        }
+      }
+      if ($content !~ /\bMIN_PERL_VERSION\s*=>\s*['"]/) {
+        my $version=Module::Install::Metadata::_extract_perl_version($main_file_content) ||
+          Module::Install::Metadata::_extract_perl_version($content);
+        if ($version) {
+          push @param,"    MIN_PERL_VERSION => '$version',\n";
+        }
       }
     }
+  } else {
+    print "VERSION_FROM not found\n";
     if ($content !~ /\bMIN_PERL_VERSION\s*=>\s*['"]/) {
-      my $version=Module::Install::Metadata::_extract_perl_version($main_file_content) ||
-        Module::Install::Metadata::_extract_perl_version($content);
+      my $version=Module::Install::Metadata::_extract_perl_version($content);
       if ($version) {
         push @param,"    MIN_PERL_VERSION => '$version',\n";
       }
     }
   }
-} else {
-  print "VERSION_FROM not found\n";
-  if ($content !~ /\bMIN_PERL_VERSION\s*=>\s*['"]/) {
-    my $version=Module::Install::Metadata::_extract_perl_version($content);
-    if ($version) {
-      push @param,"    MIN_PERL_VERSION => '$version',\n";
-    }
-  }
-}
 
-if (@resourses) {
-  my $res=join("\n",@resourses);
-  push @param,<<EOT;
+  if (@resourses) {
+    my $res=join("\n",@resourses);
+    push @param,<<EOT;
     META_MERGE => {
         resources => {
 $res
         },
     },
 EOT
-}
-my $param='';
-if (@param) {
-  $param="\n".join('',@param);
-  $param=apply_indent($param,4,$space_to_use);
-  $param=~s/\s+$//s;
-}
-$content=~s/WriteMakefile\s*\(/WriteMakefile1($param/s;
+  }
+  my $param='';
+  if (@param) {
+    $param="\n".join('',@param);
+    $param=apply_indent($param,4,$space_to_use);
+    $param=~s/\s+$//s;
+  }
+  $content=~s/WriteMakefile\s*\(/WriteMakefile1($param/s;
 
-$content=~s/[\r\n]+$//s;
-$content.="\n\n".apply_indent($compat_layer,4,$space_to_use);
-
+  $content=~s/[\r\n]+$//s;
+  $content.="\n\n".apply_indent($compat_layer,4,$space_to_use);
+}
 
 rename('Makefile.PL','Makefile.PL.bak');
-write_file('Makefile.PL',$content);
+write_file('Makefile.PL',process_file($content));
